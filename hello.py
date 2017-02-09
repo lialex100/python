53cbaeb210e8675e4e63eb578f5ee8fda7ec2c53
Alex <li.alex100@gmail.com>
2017-02-09 17:30:50 +0000

    modified:   hello.py 	api/ 	static/ 	templates/

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from flask import Flask , session, redirect, url_for, escape, request ,render_template, send_from_directory
+from flask_restful import Resource, Api
+
 app = Flask(__name__, static_url_path='/static')
+api = Api(app)
 
 @app.route('/')
 def index():
@@ -12,3 +15,12 @@ def index():
 @app.route('/login')
 def login():
     app.logger.debug('A value for debugging')
+
+
+
+
+from api.helloWorld import HelloWorld
+api.add_resource(HelloWorld, '/api')
+
+if __name__ == '__main__':
+    app.run(debug=True)
