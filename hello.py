5df51e60af6d39977b9fce221d2e16bfdd2dc6bb
Alex <li.alex100@gmail.com>
2017-02-09 15:19:08 +0000

    modified:   hello.py 	static/ 	templates/

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask , session, redirect, url_for, escape, request ,render_template, send_from_directory
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World! 45623'
-    
-@app.route('/projects/')
-def projects():
-    return 'The project pageasdasasdeasssd'
+def index():
+    return send_from_directory('static','index.html')
 
-@app.route('/about')
-def about():
-    return 'The about page'
+# @app.route('/static')
+# def static(path):
+#     return send_from_directory('static', path)
+
+@app.route('/login')
+def login():
+    app.logger.debug('A value for debugging')
